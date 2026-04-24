5bf65150bfb33dc0b764e940caf2104fe3d129c7
Srinivasan <srinivasanmurugappan@gmail.com>
2026-04-24 12:58:20 +0530

    Admin Features

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManageDoctorsComponent } from './manage-doctors/manage-doctors.component';
+import { ManagePatientsComponent } from './manage-patients/manage-patients.component';
+import { ManageAppointmentsComponent } from './manage-appointments/manage-appointments.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+ 
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard',           component: DashboardComponent },
+  { path: 'manage-doctors',      component: ManageDoctorsComponent },
+  { path: 'manage-patients',     component: ManagePatientsComponent },
+  { path: 'manage-appointments', component: ManageAppointmentsComponent },
+  { path: 'statistics',          component: StatisticsComponent }
+];
+ 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
